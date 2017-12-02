5bbe24ac1c18ea8e1d7c34b369f213a71166ef07
Mac  Tecsup <claudia.cordova@tecsup.edu.pe>
2017-12-02 16:23:07 -0500

    mostrar mapa con zoom

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -14,19 +14,35 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     var  ubicacion = CLLocationManager()
+    var contActualizaciones = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ubicacion.delegate = self
-        ubicacion.requestWhenInUseAuthorization()
-        ubicacion.showsUserLocation = true
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+            ubicacion.startUpdatingLocation()
+        }else{
+            ubicacion.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func  locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if(contActualizaciones < 1){
+            let region = MKCoordinateRegionMakeWithDistance(ubicacion.location!.coordinate, 1000, 1000)
+            mapView.setRegion(region, animated: true)
+            print("Ubicacion actualizada")
+            print(region)
+            print(contActualizaciones)
+        }else{
+            ubicacion.stopUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
